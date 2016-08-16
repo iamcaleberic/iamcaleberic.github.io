89de3d83ac4b0442af6a677f00de01ba3e580911
Eric Caleb <calebme@mail.com>
2016-08-16 12:31:13 +0100

    Added service worker offline support

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 self.addEventListener('install', function(e) {
 e.waitUntil(
-caches.open('airhorner').then(function(cache) {
+caches.open('iamcaleb').then(function(cache) {
 return cache.addAll([
-'/',
-'index.html',
-'index.html?homescreen=1',
-'?homescreen=1',
-'/css/master.css',
-'/css/semantic.min.css',
-'/js/master.js',
-'/js/typed.js',
-'/images/home.jpg'
+'./',
+'./index.html',
+'./index.html?homescreen=1',
+'./?homescreen=1',
+'../css/master.css',
+'../css/semantic.min.css',
+'../js/master.js',
+'../js/typed.js',
+'../assets/images/home.jpg'
 ]);
 })
 );
@@ -19,4 +19,9 @@ return cache.addAll([
 
 self.addEventListener('fetch', function(event) {
 console.log(event.request.url);
+event.respondWith(
+caches.match(event.request).then(function(response) {
+return response || fetch(event.request);
+})
+);
 });

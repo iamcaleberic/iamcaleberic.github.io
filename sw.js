5f787a9d00deb32962e12ad233ef3adec3731831
Eric Caleb <calebme@mail.com>
2016-08-16 11:59:33 +0100

    Added cache and fetched litseners

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open('airhorner').then(function(cache) {
+return cache.addAll([
+'/',
+'index.html',
+'index.html?homescreen=1',
+'?homescreen=1',
+'/css/master.css',
+'/css/semantic.min.css',
+'/js/master.js',
+'/js/typed.js',
+'/images/home.jpg'
+]);
+})
+);
+});
 
+
+self.addEventListener('fetch', function(event) {
+console.log(event.request.url);
+});

8a2626a4f44241d84a9c45d75d069528b11f2a36
Eric Caleb <calebme@mail.com>
2016-12-19 13:55:35 +0000

    Added Router to index.js | changed title

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,14 @@
+import { Router, Route, browserHistory } from 'react-router'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+    </Route>
+  </Router>
+  ,
   document.getElementById('root')
 );

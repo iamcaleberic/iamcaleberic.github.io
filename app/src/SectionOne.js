a5f1fe7771fe1ff2ade6739d4323921de8a97109
Eric Caleb <calebme@mail.com>
2016-12-21 15:51:36 +0000

    Modified component

---
+++ app/src/SectionOne.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Segment } from 'semantic-ui-react'
+
+class SectionOne extends Component {
+  render() {
+    return (
+        <Segment inverted>
+          <p>Yes</p>
+        </Segment>
+    );
+  }
+}
+
+export default SectionOne;

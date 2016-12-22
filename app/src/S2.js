30a857c8b42b9bfa37e3e75fafff967d616e81d2
Eric Caleb <calebme@mail.com>
2016-12-22 15:31:44 +0000

    Added Section 2 component

---
+++ app/src/S2.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Segment, Grid, Card, Icon, Image, Divider } from 'semantic-ui-react'
+
+import Stack from './Stack'
+
+export default class S2 extends Component {
+  render(){
+    return(
+      <Segment basic >
+        <Grid stackable columns='2' >
+          <Grid.Column>
+            <Card centered>
+              <Image src='https://iamcaleberic.github.io/assets/images/me.jpg' />
+              <Card.Content >
+                <Card.Header>Eric Caleb</Card.Header>
+                <Card.Meta> 2017 </Card.Meta>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='twitter' />
+                  eric_caleb
+                </a>
+                <a className="right floated">
+                  <Icon name='github' />
+                  iamcaleberic
+                </a>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Divider vertical />
+          <Grid.Column>
+            <Stack />
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    )
+  }
+}

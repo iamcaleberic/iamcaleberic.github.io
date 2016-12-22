cd8081b98e5481e3faf024043f6b0d198c67bd22
Eric Caleb <calebme@mail.com>
2016-12-22 17:41:43 +0000

    Added projects json objects

---
+++ app/src/Portfolio.js
@@ -0,0 +1,27 @@
+export const Portfolio = [
+  {'name':'Verby', 'link':'https://verby.io',
+   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
+   'stack':'Rails & Semantic UI'
+  },
+  {'name':'Sophie', 'link':'github.com', 'description': 'Yes'},
+  {'name':'Verby', 'link':'https://verby.io',
+   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
+   'stack':'Rails & Semantic UI'
+  },
+  {'name':'Verby', 'link':'https://verby.io',
+   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
+   'stack':'Rails & Semantic UI'
+  },
+  {'name':'Verby', 'link':'https://verby.io',
+   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
+   'stack':'Rails & Semantic UI'
+  },
+  {'name':'Verby', 'link':'https://verby.io',
+   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
+   'stack':'Rails & Semantic UI'
+  },
+  {'name':'Verby', 'link':'https://verby.io',
+   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
+   'stack':'Rails & Semantic UI'
+  },
+]

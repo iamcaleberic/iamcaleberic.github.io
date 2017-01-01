c10df82f21d726a05921e5708bbef225991510f4
Eric Caleb <calebme@mail.com>
2017-01-01 18:55:56 +0000

    Modify project cards

--- app/src/S3.js
+++ app/src/S3.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Segment, Header, Card } from 'semantic-ui-react'
+import { Segment, Header, Card, Icon } from 'semantic-ui-react'
 
 import { Portfolio } from './Portfolio'
 
@@ -21,7 +21,21 @@ class Project extends Component {
     const {project} = this.props
     return(
         <Card >
-          <Card.Header content={project.name} />
+          <Card.Content >
+            <Card.Header content={project.name} />
+            <Card.Meta content={project.stack} />
+            <Card.Description content={project.description} />
+          </Card.Content >
+          <Card.Content extra>
+            <a href={project.link} target="_blank">
+              <Icon name='browser' />
+              Web
+            </a>
+            <a className="right floated" href={project.github} target="_blank">
+              <Icon name='code' />
+              Code
+            </a>
+          </Card.Content>
         </Card>
     )
   }

1214512fb84af35f874bd36bac480405f640547a
Eric Caleb <calebme@mail.com>
2017-01-01 18:55:16 +0000

    Modified portfolio

--- app/src/Portfolio.js
+++ app/src/Portfolio.js
@@ -1,27 +1,59 @@
 export const Portfolio = [
-  {'name':'Verby', 'link':'https://verby.io',
+  {'name':'Verby', 'link':'https://verby.io',github: 'https://github.com/iamcaleberic/verby',
    'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
    'stack':'Rails & Semantic UI'
   },
-  {'name':'Sophie', 'link':'github.com', 'description': 'Yes'},
-  {'name':'Verby', 'link':'https://verby.io',
-   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
-   'stack':'Rails & Semantic UI'
+  {'name':'Travel Finder', 'link':'#', github: 'https://github.com/iamcaleberic/travel-finder',
+   'description': 'A terminal based application exploring Native Ruby and RSpec Testing Framework',
+   'stack':'Ruby & Rspec'
   },
-  {'name':'Verby', 'link':'https://verby.io',
-   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
-   'stack':'Rails & Semantic UI'
+  {'name':'Rover', 'link':'#', github: 'https://github.com/iamcaleberic/rover',
+   'description': 'A terminal based application exploring Native Ruby and RSpec Testing Framework',
+   'stack':'Ruby & Rspec'
   },
-  {'name':'Verby', 'link':'https://verby.io',
-   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
-   'stack':'Rails & Semantic UI'
+  {'name':'Genesis', 'link':'#',github: 'https://github.com/iamcaleberic/genesis',
+   'description': 'C terminal based application to showcase examples of C native',
+   'stack':'C & C++'
   },
-  {'name':'Verby', 'link':'https://verby.io',
-   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
-   'stack':'Rails & Semantic UI'
+  {'name':'Slither', 'link':'https://iamcaleberic.github.io/slither', github: 'https://github.com/iamcaleberic/slither',
+   'description': 'A JavaScript Canvas Game',
+   'stack':'Javascript'
   },
-  {'name':'Verby', 'link':'https://verby.io',
-   'description': 'Application working towards Monetizing writers and bloggers content and promoting quality content',
-   'stack':'Rails & Semantic UI'
+  {'name':'Servant', 'link':'#', github: 'https://github.com/iamcaleberic/servant',
+   'description': 'Javascript and Coffeescript application showcasing sass compilation using gulp',
+   'stack':' JavaScript & SASS & CoffeScript & Gulp'
   },
+  {'name':'Rebirth', 'link':'#', github: 'https://github.com/iamcaleberic/rebirth',
+   'description': 'Python terminal based application.',
+   'stack':'Python'
+  },
+  {'name':'Ghost', 'link':'#', github: 'https://github.com/shadowscript/ghost',
+   'description': 'Basic Node file and api Server',
+   'stack':'Node JS'
+  },
+  {'name':'IG', 'link':'#', github: 'https://github.com/ongair/ig',
+   'description': 'Application exploring redux with react',
+   'stack':'JavaScript, ES6 & Redux & React'
+  },
+  {'name':'London', 'link':'#', github: 'https://github.com/iamcaleberic/london',
+   'description': 'Application showing basic usage of rails with Mongo database',
+   'stack':'Rails & Mongo DB'
+  },
+  {'name':'Shadowscript', 'link':'https://shadowscript.github.io', github: 'https://github.com/shadowscript/shadowscript.github.io',
+   'description': 'Shadowscript organization page',
+   'stack':'Semantic UI'
+  },
+  {'name':'Shadowscript Docker', 'link':'#', github: 'https://github.com/shadowscript/docker-client',
+   'description': 'Shadowscript docker client',
+   'stack':'Yaml'
+  },
+  {'name':'Shadowscript Chef Cookbook', 'link':'#', github: 'https://github.com/shadowscript/chef_shadowscript',
+   'description': 'Shadowscript Chef Cookbook',
+   'stack':'Ruby & Bash & HTML'
+  },
+  {'name':'Photobeat', 'link':'#', github: 'https://github.com/iamcaleberic/photobeat',
+   'description': 'A stock photography application.',
+   'stack':'Rails & Semantic UI & Masonry JS'
+  },
+
 ]
